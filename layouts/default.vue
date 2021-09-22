efee0811d61cf71e4180dde1f3f83a3381d1a538
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-09-22 13:24:45 +0200

    build: initial nuxtJS-App
    Navbar, ESLint tests, Bootstrap

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-btn width="10%" @click="isOpenNavDrawer = !isOpenNavDrawer">
+      <v-icon class="mx-2">mdi-menu</v-icon> Navigation
+    </v-btn>
+    <v-navigation-drawer v-model="isOpenNavDrawer" app>
+      <v-list>
+        <v-list-item @click="isOpenNavDrawer = !isOpenNavDrawer">
+          <v-icon class="mx-2">mdi-menu</v-icon> Navigation
+        </v-list-item>
+        <v-list-item> test </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpenNavDrawer: false,
+    };
+  },
+};
+</script>
