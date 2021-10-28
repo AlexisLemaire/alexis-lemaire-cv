c676809ed29b76b18cc7fe4fe4f9a492282b3542
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-10-28 17:43:27 +0200

    fix: add responsive field in ajout project page test

--- tests/end-to-end/userExperience.spec.js
+++ tests/end-to-end/userExperience.spec.js
@@ -9,10 +9,11 @@ describe("userExperience", () => {
   it("should seek for navbar links and visit ajout projet page", () => {
     cy.get("nav a span").contains("CV");
     cy.get("nav a span").contains("Ajout projet").click();
+    cy.visit("https://alexis-lemaire-cv.herokuapp.com/addProject");
+    cy.get("form");
   });
 
   it('should check ajout projet form is working', () => {
-    cy.get("form");
     cy.get("input").eq(0).type("FakeKey");
     cy.get("input").eq(1).type("FakeTitle");
     cy.get("input").eq(2).type("FakeClient");
@@ -22,7 +23,8 @@ describe("userExperience", () => {
     cy.get("input").eq(6).type("FakeGithubAPI");
     cy.get("input").eq(7).type("FakeFrontendTechs");
     cy.get("input").eq(8).type("FakeBackendTechs");
-    cy.get(".v-select__slot").type("Maintenu{enter}");
+    cy.get(".v-select__slot").eq(0).type("Maintenu{enter}");
+    cy.get(".v-select__slot").eq(1).type("Non{enter}");
     cy.get(".v-picker td").eq(25).click();
     cy.get("form button").contains("Ajouter").click();
     cy.get(".v-alert");
